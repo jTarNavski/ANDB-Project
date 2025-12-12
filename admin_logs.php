83d54098c991adb7717809745cb71764ba79368a
Jakub Tarnawski <s197661@student.pg.edu.pl>
2025-12-12 16:44:37 +0100

    Adding basic html page for admin logs

---
+++ admin_logs.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Activity Logs</title>
+</head>
+<body>
+    <div>
+        <h2>System Activity Logs</h2>
+        <a href="admin.php">Back to Users</a>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Date/Time</th>
+                <th>Username</th>
+                <th>Role</th>
+                <th>Activity</th>
+            </tr>
+        </thead>
+    </table>
+</body>
+</html>
