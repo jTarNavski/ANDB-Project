dfd546930e53e40a05c212179c5770d20f067b62
mateusz <my_name@example.com>
2025-12-13 15:48:28 +0100

    Adding php logic

--- logout.php
+++ logout.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
 
+// Check if a user was actually logged in before logging them out
+if (isset($_SESSION['user_id'])) {
+    try {
+        $userId = $_SESSION['user_id'];
+        $username = $_SESSION['user_login'];
+        $role = $_SESSION['role'];
 
+        // Insert LOGOUT event
+        $sql = "INSERT INTO ActivityLog (UserId, Username, UserRole, ActivityType) VALUES (?, ?, ?, 'LOGOUT')";
+        $stmt->execute([$userId, $username, $role]);
+    } catch (Exception $e) {
+    }
+}
 
 // Destroy session and redirect
 session_destroy();
